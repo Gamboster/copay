36ed9962c374843dfe7c7f7322dee5c005600255
Gabriel Bazán <gabrielbazan7@hotmail.com>
2018-03-26 14:43:51 -0300

    Avoid receiving funds in a no backed up wallet

--- src/pages/paper-wallet/paper-wallet.ts
+++ src/pages/paper-wallet/paper-wallet.ts
@@ -33,7 +33,6 @@ export class PaperWalletPage {
   public passphrase: string;
   public privateKey: string;
   public balanceSat: number;
-  public singleWallet: boolean;
   public noMatchingWallet: boolean;
   public balanceHidden: boolean;
   public error: boolean;
@@ -74,7 +73,10 @@ export class PaperWalletPage {
       onlyComplete: true,
       network: 'livenet',
     });
-    this.singleWallet = this.wallets.length == 1;
+
+    this.wallets = _.filter(_.clone(this.wallets), (wallet: any) => {
+      return !wallet.needsBackup;
+    });
 
     if (!this.wallets || !this.wallets.length) {
       this.noMatchingWallet = true;
